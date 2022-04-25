95a08ee340f77de9fb4216fa29677daaa5c3f072
Patrick Liang <pl204511194@yahoo.com>
2022-04-25 13:53:23 -0400

    added little cool effect

--- src/components/One.js
+++ src/components/One.js
@@ -5,8 +5,7 @@ function One() {
     <div id="group1" className="parallax-group">
     <div className="parallax-layer parallax-layer-base">
       <div className="title">
-        <h1>Patrick Liang</h1>
-        <h3>Fullstack Software Engineer</h3>
+        <h1 class="line-1 anim-typewriter">Patrick Liang Fullstack Software Engineer...</h1>
       </div>
     </div>
   </div>
--- src/components/Six.js
+++ src/components/Six.js
@@ -4,7 +4,6 @@ function Six() {
   return (
     <div id="group6" className="parallax-group">
     <div className="parallax-layer parallax-layer-back">
-      <div className="title" style={{color: 'white'}}>Background Layer</div>
     </div>
     <div className="parallax-layer parallax-layer-base">
 

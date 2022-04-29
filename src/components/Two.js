e415bde0f25caf324220d65e10cb20d1db797246
Patrick Liang <pl204511194@yahoo.com>
2022-04-29 17:40:03 -0400

    updated bio

--- src/components/Two.js
+++ src/components/Two.js
@@ -8,12 +8,7 @@ function Two() {
       <div className="profile">
       <img src={profilepic} alt=""/>
       <div className="profile-description">
-      <span>Fullstack Software Engineer in New York City who specializes in React, Ruby, Ruby on Rails, PostgreSQL, Javascript, HTML, CSS. 
-        Coming from an Electrical Engineering background, I am always avid in being hands on and learning new technologies. 
-        I chose to abandon the path of tinkering with hardware to transition to software to fully expand on my creativity that 
-        I couldn't with hardware. Bringing my problem solving background with me, I have shifted my career path to not just be a software 
-        developer/engineer but to be an effective critical thinker simultaneously. Outside of all this, I like to crack lame jokes, make memes, 
-        and having a good time with people :)
+      <span>Graduating with a bachelor’s degree in Electrical Engineering, and having worked in the healthcare sector for 3 years, I am always avid in being hands on, learning new technologies and collaborating with others. I chose to abandon the path of tinkering with hardware to transition to software to fully expand on my creativity that I couldn't with hardware. Bringing my problem solving background and a team oriented mindset with me, I have shifted my career path to not just be a software developer/engineer but to be an effective critical thinker simultaneously. Outside of all this, I like to crack lame jokes, make memes, and having a good time with people :)
       </span>
       </div>
       </div>

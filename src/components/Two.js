e77233572b90d06b24dd0aa62886e6625d7a5862
Patrick Liang <pl204511194@yahoo.com>
2022-04-29 18:02:29 -0400

    corrected typo

--- src/components/Two.js
+++ src/components/Two.js
@@ -8,7 +8,7 @@ function Two() {
       <div className="profile">
       <img src={profilepic} alt=""/>
       <div className="profile-description">
-      <span>Graduating with a bachelor’s degree in Electrical Engineering, and having worked in the healthcare sector for 3 years, I am always avid in being hands on, learning new technologies and collaborating with others. I chose to abandon the path of tinkering with hardware to transition to software to fully expand on my creativity that I couldn't with hardware. Bringing my problem solving background and a team oriented mindset with me, I have shifted my career path to not just be a software developer/engineer but to be an effective critical thinker simultaneously. Outside of all this, I like to crack lame jokes, make memes, and having a good time with people :)
+      <span>Graduated with a bachelor’s degree in Electrical Engineering, and having worked in the healthcare sector for three years, I am always avid in being hands on, learning new technologies and collaborating with others. I chose to abandon the path of tinkering with hardware to transition to software to fully expand on my creativity that I couldn't with hardware. Bringing my problem solving background and a team oriented mindset with me, I have shifted my career path to not just be a software developer/engineer but to be an effective critical thinker simultaneously. Outside of all this, I like to crack lame jokes, make memes, and having a good time with people :)
       </span>
       </div>
       </div>
